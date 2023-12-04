1e80c40326e4e7be786c21beb639162432bb4d68
Abhijit Darekar <darekarabhijit63@gmail.com>
2023-12-04 13:31:04 +0530

    configuation file

---
+++ documentGPT/cfg.py
@@ -0,0 +1,9 @@
+class CGF:
+    open_ai_key = 'afas'
+    source_directory = '../inputs'
+    persistent_directory = 'db'
+    model_type = "openai" # openai or hf.
+    hf_model_name = ""
+    hf_text_embedd_model_name = ''
+    retriever_search_kwargs=''
+
